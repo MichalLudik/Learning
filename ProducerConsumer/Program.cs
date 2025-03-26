b3de952c3c993b63e44f1a0abdcff69545dac798
Michal Ludik <michal.ludik@gmail.com>
2025-03-26 20:19:39 +0100

    Producer Consumer intro.

---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ProducerConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // one producer one consumer
+            var producer = new PCQueue();
+
+            Action action;
+            EnqueueWorkWithRandomDelay();
+
+            Console.WriteLine(producer.Queue.Count());
+
+
+            void EnqueueWorkWithRandomDelay()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(TimeSpan.FromMilliseconds(random.Next(100, 2000)));
+                    action = () => Console.WriteLine($"Attempt {i}");
+                    producer.EnqueueWork(action);
+                }
+            }
+        }
+    }
+
+    public class PCQueue
+    {
+        public Queue<Action> Queue = new Queue<Action>();
+
+        public void EnqueueWork(Action action)
+        {
+            Queue.Enqueue(action);
+        }
+    }
+}
