0ea3dcfcf429f044872f9155168abaa4947b47d6
michal <michal.ludik@gmail.com>
2025-03-26 21:10:49 +0100

    Basic producer-consumer scenario

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -1,39 +1,78 @@
-﻿namespace ProducerConsumer
+﻿using System.Diagnostics;
+
+namespace ProducerConsumer
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static Stopwatch sw = new Stopwatch();
+
+        static async Task Main(string[] args)
         {
+            sw.Start();
+
             // one producer one consumer
-            var producer = new PCQueue();
+            var pcQueue = new PCQueue();
 
             Action action;
-            EnqueueWorkWithRandomDelay();
+            var t1 = Task.Run(() => EnqueueWorkWithRandomDelay());
+            var t2 = Task.Run(() => pcQueue.DequeueWork());
 
-            Console.WriteLine(producer.Queue.Count());
+            await t1;
+            await t2;
+
+            Console.WriteLine(pcQueue.Queue.Count());
 
 
-            void EnqueueWorkWithRandomDelay()
+            async Task EnqueueWorkWithRandomDelay()
             {
                 Random random = new Random();
 
                 for (int i = 0; i < 10; i++)
                 {
-                    Thread.Sleep(TimeSpan.FromMilliseconds(random.Next(100, 2000)));
-                    action = () => Console.WriteLine($"Attempt {i}");
-                    producer.EnqueueWork(action);
+                    int x = i;
+                    Thread.Sleep(TimeSpan.FromMilliseconds(random.Next(100, 500)));
+                    Console.WriteLine($"[{sw.ElapsedMilliseconds}] Adding action no. {x}");
+
+                    action = () => Console.WriteLine($"[{sw.ElapsedMilliseconds}] Performing action no. {x}");
+                    pcQueue.EnqueueWork(action);
                 }
+
+                pcQueue.shouldProcess = false;
             }
         }
-    }
 
-    public class PCQueue
-    {
-        public Queue<Action> Queue = new Queue<Action>();
 
-        public void EnqueueWork(Action action)
+        public class PCQueue
         {
-            Queue.Enqueue(action);
+            public bool shouldProcess = true;
+
+            public Queue<Action> Queue = new Queue<Action>();
+
+            public void EnqueueWork(Action action)
+            {
+                Queue.Enqueue(action);
+            }
+
+            public async Task DequeueWork()
+            {
+                while (shouldProcess)
+                {
+                    Console.WriteLine($"[{sw.ElapsedMilliseconds}] --- DEQUEUE ---");
+
+                    await Task.Delay(1000);
+
+                    Dequeue();
+                }
+            }
+
+            private void Dequeue()
+            {
+                while (Queue.TryDequeue(out var item))
+                {
+                    //Console.WriteLine("Dequeuing action...");
+                    item();
+                }
+            }
         }
     }
 }
