2ff46f7feda218d24aad0e628e8bfc8036a0deaf
michal <michal.ludik@gmail.com>
2025-04-25 13:39:13 +0200

    Blocking Collection

---
+++ ProducerConsumerBlockingCollection/Blocking.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Concurrent;
+
+namespace ProducerConsumerBlockingCollection
+{
+    internal class Blocking
+    {
+        static void Main(string[] args)
+        {
+
+            BlockingCollection<int> _queue1 = new(5);
+
+            _queue1.TryTake(out var item);
+
+            var prod = new Prod();
+
+            var t1 = Task.Run(prod.Adding);
+            var t1x = Task.Run(prod.Adding);
+            var t1y = Task.Run(prod.Adding);
+            var t1z = Task.Run(prod.Adding);
+
+            var t2 = Task.Run(prod.Consuming);
+            var t3x = Task.Run(prod.Consuming);
+            var t3y = Task.Run(prod.Consuming);
+
+            Task.WaitAll(t1, t1x, t1y, t1z);
+            prod.FinishAdding();
+
+            Task.WaitAll(t2, t3x, t3y);
+        }
+    }
+
+    class Prod
+    {
+        BlockingCollection<int> _queue = new(5);
+
+
+        public void Adding()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Adding item '{i}'");
+                _queue.Add(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void FinishAdding()
+        {
+            Thread.Sleep(10000);
+            _queue.CompleteAdding();
+        }
+
+        public void Consuming()
+        {
+            while (!_queue.IsCompleted) // only when CompleteAdding was called and _queue length is 0
+            {
+                Console.WriteLine("..");
+                try
+                {
+                    _queue.TryTake(out int itemTakenAndRemoved);
+
+                    Console.WriteLine($"Consuming item '{itemTakenAndRemoved}'");
+
+                    Thread.Sleep(100);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Something bad happened.", ex);
+                }
+
+            }
+        }
+    }
+}
---
+++ ProducerConsumerQueueTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+
+namespace ProducerConsumerQueueTypes
+{
+    internal class Program
+    {
+        static BlockingCollection<int> concurrentQueue = new(new ConcurrentQueue<int>());
+        static BlockingCollection<int> concurrentStack = new(new ConcurrentStack<int>());
+
+        static void Main(string[] args)
+        {
+            int[] col = Enumerable.Range(0, 10).ToArray();
+
+            AddItemsToCollection(concurrentQueue, col);
+            AddItemsToCollection(concurrentStack, col);
+
+            Console.WriteLine("Dequeuing Concurrent Queue");
+            Dequeue(concurrentQueue);
+
+            Console.WriteLine("Dequeuing Concurrent Stack");
+            Dequeue(concurrentStack);
+        }
+
+        static void AddItemsToCollection(BlockingCollection<int> collection, int[] items)
+        {
+            foreach (int item in items)
+            {
+                collection.Add(item);
+            }
+        }
+
+        static void Dequeue(BlockingCollection<int> collection)
+        {
+            while (collection.Count > 0)
+            {
+                if (collection.TryTake(out int item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
